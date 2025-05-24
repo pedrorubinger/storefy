2c6e8a067060f46fc1474e9bd256e9b8c3cc4219
Pedro Rubinger <pedro.rubinger@gmail.com>
2025-05-24 17:33:38 -0300

    style: add the new app colors

--- constants/Theme.ts
+++ constants/Theme.ts
@@ -2,18 +2,40 @@ import { ISupportedTheme } from "@/interfaces/Theme";
 
 export const AppTheme: ISupportedTheme = "light";
 
-const tintColorLight = "#0a7ea4";
-
 const Theme = {
   colors: {
     light: {
-      text: "#11181C",
+      primary: "#249c70",
+
       background: "#fff",
-      tint: tintColorLight,
-      link: "#0a7ea4",
-      icon: "#687076",
-      tabIconDefault: "#687076",
-      tabIconSelected: tintColorLight,
+
+      black: "#1A1A1A",
+
+      blue80: "#f3f8ff",
+
+      grey80: "##f1f1f1",
+      grey100: "#F5F5F5",
+      grey150: "#FCFCFC",
+      grey200: "#E5E5E5",
+      grey300: "#B3B3B3",
+      grey500: "#808080",
+      grey700: "#484848",
+
+      green400: "#29B985",
+
+      koamaru100: "#31385b",
+      koamaru200: "#373f66",
+      koamaru300: "#31385b",
+
+      orange100: "#F1C27B",
+      orange200: "#FFB07F",
+      orange400: "#F3AA60",
+
+      red400: "#FE5C51",
+      red300: "#ff7b73",
+      red200: "#FF9891",
+
+      white: "#FFFFFF",
     },
   },
   fonts: {
