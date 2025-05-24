f2bd15f26488a7e3d5828fc41990332d25565879
Pedro Rubinger <pedro.rubinger@gmail.com>
2025-05-24 18:23:04 -0300

    feat: add Typography component

---
+++ components/Typography.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Text, TextProps } from "react-native";
+
+import { useTheme } from "@/hooks/useTheme";
+import { ColorName, FontName, FontSize } from "@/interfaces/Theme";
+
+export interface TypographyProps {
+  font?: FontName;
+  color?: ColorName;
+  size?: FontSize;
+}
+
+interface Props extends TypographyProps, TextProps {}
+
+export const Typography: React.FC<Props> = ({
+  children,
+  font = "default400",
+  size = "md",
+  color = "primary",
+  ...rest
+}) => {
+  const theme = useTheme();
+
+  return (
+    <Text
+      style={{
+        fontFamily: theme.fonts[font],
+        fontSize: theme.fontSizes[size],
+        color: theme.colors[color],
+      }}
+      {...rest}
+    >
+      {children}
+    </Text>
+  );
+};
