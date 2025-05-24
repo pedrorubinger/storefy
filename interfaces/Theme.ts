67e1f70ba70f1135d15c278876a9c467d790455a
Pedro Rubinger <pedro.rubinger@gmail.com>
2025-05-24 18:20:08 -0300

    feat: add Theme interfaces

--- interfaces/Theme.ts
+++ interfaces/Theme.ts
@@ -1,1 +1,7 @@
+import { getTheme } from "@/constants/Theme";
+
 export type ISupportedTheme = "light";
+export type ColorName = keyof ReturnType<typeof getTheme>["colors"];
+export type ColorValue = ReturnType<typeof getTheme>["colors"][ColorName];
+export type FontName = keyof ReturnType<typeof getTheme>["fonts"];
+export type FontSize = keyof ReturnType<typeof getTheme>["fontSizes"];
