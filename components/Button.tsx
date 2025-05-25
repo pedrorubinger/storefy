733d985e41adb9571ebda41f025886a72844b344
Pedro Rubinger <pedro.rubinger@gmail.com>
2025-05-24 21:45:22 -0300

    feat: add button component

---
+++ components/Button.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { TouchableOpacity, TouchableOpacityProps } from "react-native";
+
+import { Typography } from "@/components/Typography";
+import { useTheme } from "@/hooks/useTheme";
+import { ColorName, FontName, FontSize } from "@/interfaces/Theme";
+import { styles } from "@/styles/button.styles";
+
+export interface ButtonProps extends TouchableOpacityProps {
+  font?: FontName;
+  size?: FontSize;
+  color?: ColorName;
+  backgroundColor?: ColorName;
+  borderColor?: ColorName;
+  icon?: React.ReactNode;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  font = "default400",
+  size = "md",
+  color = "white",
+  backgroundColor = "black",
+  borderColor = "black",
+  children,
+  style,
+  icon,
+  ...rest
+}) => {
+  const theme = useTheme();
+
+  return (
+    <TouchableOpacity
+      style={[
+        styles.button,
+        {
+          backgroundColor: theme.colors[backgroundColor],
+          borderColor: theme.colors[borderColor],
+        },
+        style,
+      ]}
+      {...rest}
+    >
+      <Typography font={font} size={size} color={color}>
+        {children}
+      </Typography>
+      {}
+      {icon}
+    </TouchableOpacity>
+  );
+};
---
+++ styles/button.styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+import { getTheme } from "@/constants/Theme";
+
+const { radius, spacing } = getTheme();
+
+export const styles = StyleSheet.create({
+  button: {
+    borderWidth: 1,
+    paddingHorizontal: spacing.md,
+    paddingVertical: spacing.sm + 4,
+    borderRadius: radius.md,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    textAlign: "center",
+  },
+});
